dc828049f7da282b628af6dd4621d1d3a0c6b69f
cold-noodle-king <bgenius02@gmail.com>
2024-09-02 10:39:51 +0900

    Initial commit

---
+++ src/main/java/net/datasa/sharyproject/SharyProjectApplication.java
@@ -0,0 +1,13 @@
+package net.datasa.sharyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/datasa/sharyproject/SharyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.datasa.sharyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
