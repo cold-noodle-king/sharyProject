0e9142c1ee623d16aea6a7519f9f69382f256584
cold-noodle-king <bgenius02@gmail.com>
2024-09-02 11:41:52 +0900

    폴더 세분화 및 시큐리티 파일 추가 11:41

--- src/main/java/net/datasa/sharyproject/Controller/InfoController.java
+++ src/main/java/net/datasa/sharyproject/Controller/InfoController.java
@@ -1,4 +1,4 @@
-package net.datasa.sharyproject;
+package net.datasa.sharyproject.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
