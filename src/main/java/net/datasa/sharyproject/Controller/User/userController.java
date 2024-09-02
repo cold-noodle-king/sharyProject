adaf10ecf6e735850d0ba01b181086f9d7f14709
hyunjikwak <guswl9105@gmail.com>
2024-09-02 17:44:02 +0900

    9/2 오후 5:43 header.html경로수정 -곽현지

--- src/main/java/net/datasa/sharyproject/Controller/User/userController.java
+++ src/main/java/net/datasa/sharyproject/Controller/User/userController.java
@@ -1,9 +1,28 @@
 package net.datasa.sharyproject.Controller.User;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("user")
 @Controller
 public class userController {
 
-    //@GetMapping("login")
+    /**
+     * 로그인
+     * @return 로그인 페이지 html
+     */
+    @GetMapping("login")
+    public String login() {
+        return "user/loginForm";
+    }
+
+    /**
+     * 회원가입
+     * @return 회원가입 페이지 html
+     */
+    @GetMapping("join")
+    public String join() {
+        return "user/joinForm";
+    }
 }
