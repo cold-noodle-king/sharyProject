da27d83d5968c70176d149cdc80ab9a2bb805ace
guswns1347 <guswns1347@gmail.com>
2024-09-02 10:49:51 +0900

    controller 수정 오전 11:31 커밋

---
+++ src/main/java/net/datasa/sharyproject/InfoController.java
@@ -0,0 +1,13 @@
+package net.datasa.sharyproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+    @GetMapping({"", "/"})
+    public String info() {
+        return "home"; // 나중에 info페이지 만들때 경로바꾸기
+    }
+}
+
---
+++ src/main/java/net/datasa/sharyproject/userController/userController.java
@@ -0,0 +1,10 @@
+package net.datasa.sharyproject.userController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class userController {
+
+    //@GetMapping("login")
+}
