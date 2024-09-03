a2e5e95d5264cd95b7736fe0e0990350c3051f3d
hyunjikwak <guswl9105@gmail.com>
2024-09-03 14:58:48 +0900

    9/3 오후 2:57 memberRepository 추가 곽현지

---
+++ src/main/java/net/datasa/sharyproject/Security/AuthenticatedUserDetailsService.java
@@ -0,0 +1,43 @@
+package net.datasa.sharyproject.Security;
+
+import jakarta.persistence.EntityNotFoundException;
+import net.datasa.sharyproject.domain.entity.Member.MemberEntity;
+import net.datasa.sharyproject.repository.Member.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AuthenticatedUserDetailsService implements UserDetailsService {
+
+	private final BCryptPasswordEncoder passwordEncoder; //passwordEncoder 변수명임
+	private final MemberRepository memberRepository;
+
+	@Override		 //load가 DB에서 유저네임 기준으로 가져옴
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		MemberEntity memberEntity = memberRepository.findById(username)
+				.orElseThrow(()->new EntityNotFoundException("아이디가 없습니다."));
+
+		log.debug("조회정보 : {}", memberEntity);
+
+		AuthenticatedUser user = AuthenticatedUser.builder()
+				.memberId(username)
+				.password(memberEntity.getPassword())
+				.nickname(memberEntity.getNickname()) //선택적 필드
+				.roleName(memberEntity.getRoleName())// 권한 설정
+				.build();
+		log.debug("인증정보 : {}", user);
+
+		return user; //Spring Security에서 사용될 UserDetails 객체 반환
+	}
+
+}
+
---
+++ src/main/java/net/datasa/sharyproject/domain/dto/Member/MemberDTO.java
@@ -0,0 +1,39 @@
+package net.datasa.sharyproject.domain.dto.Member;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+
+    private String memberId;
+
+    private String password;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String fullName;
+
+    private String nickname;
+
+    private String gender;
+
+    private LocalDate birthdate;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private String roleName;
+}
