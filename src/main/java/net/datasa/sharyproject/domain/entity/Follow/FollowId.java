81ac594b53bd8e32a7b5730e19b5f30c7ea0dc3c
younjolee <yjlee5634@gmail.com>
2024-09-03 10:22:19 +0900

    20240902 follow 내용 생성

--- src/main/java/net/datasa/sharyproject/domain/entity/Follow/FollowId.java
+++ src/main/java/net/datasa/sharyproject/domain/entity/Follow/FollowId.java
@@ -1,8 +1,6 @@
 package net.datasa.sharyproject.domain.entity.Follow;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinColumns;
-import jakarta.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
