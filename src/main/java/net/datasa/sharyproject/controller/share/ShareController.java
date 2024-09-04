a22a7cadbd4710bc395bc7a6491c09520121370d
cold-noodle-king <bgenius02@gmail.com>
2024-09-04 20:56:13 +0900

    /공유 다이어리 페이지 추가 및 컨트롤러 수  :56

--- src/main/java/net/datasa/sharyproject/controller/share/ShareController.java
+++ src/main/java/net/datasa/sharyproject/controller/share/ShareController.java
@@ -1,11 +1,19 @@
 package net.datasa.sharyproject.controller.share;
 
+import lombok.AllArgsConstructor;
+import net.datasa.sharyproject.domain.dto.personal.CoverTemplateDTO;
+import net.datasa.sharyproject.service.cover.CoverTemplateService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Controller
 @RequestMapping("share")
 public class ShareController {
+
+    private final CoverTemplateService coverTemplateService;
 
     @GetMapping("main")
     public String viewMain() {
@@ -22,6 +30,21 @@ public class ShareController {
     public String joined() {
 
         return "share/joinedList";
+    }
+
+    @GetMapping("joinedDiary")
+    public String joinedDiary() {
+        return "share/joinedDiary";
+    }
+
+    @GetMapping("categorySelect")
+    public String categorySelect() {
+        return "share/CategorySelect";
+    }
+
+    @GetMapping("cover")
+    public String cover() {
+        return "share/CoverSelect";
     }
 
     @GetMapping("newNote")
@@ -46,5 +69,21 @@ public class ShareController {
         return "share/main";
     }
 
+    @GetMapping("infoUpdate")
+    public String infoUpdate() {
+
+        return "share/infoUpdate";
+    }
+
+    @GetMapping("viewMember")
+    public String viewMember() {
+        return "share/viewMember";
+    }
+
+    @GetMapping("getCoverTemplates")
+    @ResponseBody
+    public List<CoverTemplateDTO> getCoverTemplates() {
+        return coverTemplateService.getCoverTemplates(); // 커버 템플릿 리스트 반환
+    }
 
 }
