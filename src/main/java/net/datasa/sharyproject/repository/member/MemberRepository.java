cf31a0dc8a9466aac558e573b153dcb9a46e9d27
younjolee <yjlee5634@gmail.com>
2024-09-10 21:05:51 +0900

    20240910 로그인한 user기준으로 팔로우, 팔로우 취소 구현

--- src/main/java/net/datasa/sharyproject/repository/member/MemberRepository.java
+++ src/main/java/net/datasa/sharyproject/repository/member/MemberRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, String> {
-    Optional<Object> findByNickname(String searchNick);
-    // 모든 회원을 조회하기 위한 findAll 메서드는 JpaRepository에 의해 자동으로 제공됩니다.
+    Optional<MemberEntity> findByNickname(String searchNick);
 }
