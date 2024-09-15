78d672496889cc936aad5e81548e1e61936a6d92
cold-noodle-king <bgenius02@gmail.com>
2024-09-15 23:32:34 +0900

    공유 다이어리 html 수정 및 생성 다이어리 관리 페이지 및 정보수정 페이지 작성

--- src/main/java/net/datasa/sharyproject/repository/share/ShareDiaryRepository.java
+++ src/main/java/net/datasa/sharyproject/repository/share/ShareDiaryRepository.java
@@ -1,9 +1,18 @@
 package net.datasa.sharyproject.repository.share;
 
+import net.datasa.sharyproject.domain.entity.personal.PersonalDiaryEntity;
 import net.datasa.sharyproject.domain.entity.share.ShareDiaryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShareDiaryRepository extends JpaRepository<ShareDiaryEntity, Integer> {
+    /**
+     * 특정 회원의 모든 다이어리를 조회합니다.
+     * @param memberId 회원 ID
+     * @return 회원의 개인 다이어리 리스트
+     */
+    List<ShareDiaryEntity> findByMember_MemberId(String memberId);
 }
