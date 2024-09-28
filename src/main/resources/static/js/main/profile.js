2082fc3f0235cacaa0f2a57a7be8e6e1d5c0d60f
hyunjikwak <guswl9105@gmail.com>
2024-09-28 22:22:31 +0900

    모달 프로필 완료

--- src/main/resources/static/js/main/profile.js
+++ src/main/resources/static/js/main/profile.js
@@ -1,1 +1,21 @@
 // 메인페이지 헤더 프로필 부분 js 파일 헤더 html 아작스여기다가
+$(document).on('click', '#profileModalTrigger', function () {
+    $.ajax({
+        url: '/mypage/profile/modal',
+        method: 'GET',
+        success: function (data) {
+            console.log("응답 데이터:", data);  // 응답 데이터를 확인합니다.
+            $('#profileImage').attr('src', data.profilePicture);
+            $('#profileNickname').text(data.nickname);
+            $('#profileMent').text(data.ment);
+            $('#profileModal').modal('show');  // 모달 열기
+        },
+        error: function (error) {
+            console.log("프로필 정보를 불러오는데 실패했습니다.", error);
+        }
+    });
+});
+
+$(document).on('click', '#profileModalTrigger', function () {
+    console.log("프로필 버튼이 클릭되었습니다.");  // 클릭 이벤트가 트리거되는지 확인
+});
